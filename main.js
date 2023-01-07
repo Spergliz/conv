6fea34cd001b0eef25c371672ed379f4236d3b01
liz <97650879+Spergliz@users.noreply.github.com>
2023-01-07 13:00:46 -0700

    wfa

---
+++ main.js
@@ -0,0 +1,8 @@
+let PoundsE1 = document.getElementById("pounds")
+let EGe1 = document.getElementById("KG")
+document.getElementById("clicker").addEventListener("click",conv)
+
+function conv(){
+    let A = PoundsE1/2.2 
+    EGe1.innerHTML = 
+}
