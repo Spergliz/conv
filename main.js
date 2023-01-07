4b2c012b276f81e184d48ffdfaf15a4b302b4e37
liz <97650879+Spergliz@users.noreply.github.com>
2023-01-07 13:07:11 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let PoundsE1 = document.getElementById("pounds")
-let EGe1 = document.getElementById("KG")
-document.getElementById("clicker").addEventListener("click",conv)
+let outputE1 = document.getElementById("KG")
+document.getElementById("clicker").addEventListener("click", conv)
 
+// WHY ISNT THIS F****** WORKING 
+// omg i forgot to add a .value
 function conv(){
-    let A = PoundsE1/2.2 
-    EGe1.innerHTML = 
+    let A = PoundsE1.value / 2
+    let B = A
+    outputE1.innerHTML = B
 }
